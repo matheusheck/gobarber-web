a4e6a264a32fe23079e205389df099a2a438eaad
Matheus Heck <matheus@conduite.tv>
2020-09-02 00:29:31 +0200

    fix validation inputs logup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
-import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import * as yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Background, Content } from './styles';
@@ -10,14 +11,18 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const HandleSubmit = useCallback(async (data: object) => {
     try {
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string()
+      console.log(data);
+      const schema = yup.object().shape({
+        name: yup.string().required('Nome obrigatório'),
+        email: yup
+          .string()
           .required('E-mail obrigatório')
           .email('Digite um email válido'),
-        password: Yup.string().min(6, 'Mínimo de 6 dígitos'),
+        password: yup.string().min(6, 'No mínimo de 6 dígitos'),
       });
 
       await schema.validate(data, {
@@ -33,13 +38,13 @@ const SignUp: React.FC = () => {
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <Form onSubmit={HandleSubmit}>
+        <Form ref={formRef} onSubmit={HandleSubmit}>
           <h1>Faça seu cadastro</h1>
-          <Input icon={FiUser} name="Name" placeholder="Nome" />
-          <Input icon={FiMail} name="E-mail" placeholder="E-mail" />
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
           <Input
             icon={FiLock}
-            name="Password"
+            name="password"
             type="password"
             placeholder="Senha"
           />
