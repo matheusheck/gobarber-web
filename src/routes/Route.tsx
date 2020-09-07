a79c0cc6f6e5417e94f6945c79600bb36a2ae15f
Matheus Heck <matheus@conduite.tv>
2020-09-07 19:44:47 +0200

    redirect auth user from login to dashboard

---
+++ src/routes/Route.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouterProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/Auth';
+
+interface RoteProps extends ReactDOMRouterProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RoteProps> = ({
+  isPrivate = false,
+  location,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={() => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
