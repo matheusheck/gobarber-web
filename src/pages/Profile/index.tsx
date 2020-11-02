382b82f4d2f8734c5edf1c0a2ab7f73da1b22321
Matheus Heck <matheus@conduite.tv>
2020-11-02 13:24:13 +0100

    create profile wip

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,102 @@
+import React, { useCallback, useRef } from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import api from '../../services/api';
+import { useToast } from '../../hooks/Toast';
+
+import logoImg from '../../assets/logo.svg';
+import { Container, Background, Content, AnimationContainer } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = yup.object().shape({
+          name: yup.string().required('Nome obrigatório'),
+          email: yup
+            .string()
+            .required('E-mail obrigatório')
+            .email('Digite um email válido'),
+          password: yup.string().min(6, 'No mínimo de 6 dígitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+        addToast({
+          type: 'success',
+          title: 'Sucesso no cadastro!',
+          description: 'Você já pode fazer seu login no GoBarber!',
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na cadastro',
+          description: 'Houve um erro no cadastr. Tente novamente.',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
+            <Input icon={FiUser} name="name" placeholder="Nome" />
+            <Input icon={FiMail} name="email" placeholder="E-mail" />
+            <Input
+              icon={FiLock}
+              name="password"
+              type="password"
+              placeholder="Senha"
+            />
+            <Button type="submit">Cadastrar</Button>
+          </Form>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para login
+          </Link>
+        </AnimationContainer>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
